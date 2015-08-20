4ba72df2b02eed6af10aaef3446f801b80572482
ddm <dimitri.delmarmol@gmail.com>
2015-08-20 22:50:46 +0200

    Clean before build

--- platforms/esp8266/make.sh
+++ platforms/esp8266/make.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-SDK=docker.cesanta.com:5000/esp8266-build-oss:1.3.0-cesanta
+#SDK=docker.cesanta.com:5000/esp8266-build-oss:1.3.0-cesanta
+SDK=cesanta/esp8266-build-oss:1.3.0-cesanta
 
 V7DIR=$(dirname $(dirname $PWD))
 DIR=/cesanta/${PWD##$V7DIR}
 # Note: "//" in "//bin/bash" is a workaround for a boot2docker bug on Windows.
 exec docker run --rm -it -v /$V7DIR:/cesanta ${SDK} //bin/bash -c \
-  "cd $DIR && make $@ && python ./tools/showbreakdown.py"
+  "cd $DIR && make clean && make $@ && python ./tools/showbreakdown.py"
